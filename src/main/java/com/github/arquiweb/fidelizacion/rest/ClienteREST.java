d645badf8d769525f42312f695bcf4dd798fc7c7
Javier Heisecke <javierheiseckee@gmail.com>
2020-09-07 23:43:30 -0400

    Agrega controller para borrado de clientes

--- src/main/java/com/github/arquiweb/fidelizacion/rest/ClienteREST.java
+++ src/main/java/com/github/arquiweb/fidelizacion/rest/ClienteREST.java
@@ -2,9 +2,11 @@ package com.github.arquiweb.fidelizacion.rest;
 
 import javax.inject.Inject;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
@@ -30,6 +32,16 @@ public class ClienteREST {
     public Response crear(Cliente p) {
         this.clienteDAO.agregar(p);
         return Response.ok().build();
-
+    }
+    
+    @DELETE
+    @Path("/{id}")
+    public Response eliminar(@PathParam(value = "id") Integer id) {
+    	try {
+	        this.clienteDAO.eliminar(id);
+	        return Response.ok().build();
+    	}catch (Exception e) {
+    		return Response.serverError().build();
+		}
     }
 }

e698fc959a5d095062e31031ed6f9036c2399480
Jazmin Villagra <jazvillagra.jv@gmail.com>
2020-09-08 22:22:23 -0400

    Agrega DAO de Vencimiento Puntos

---
+++ src/main/java/com/github/arquiweb/fidelizacion/ejb/VencimientoPuntosDAO.java
@@ -0,0 +1,32 @@
+package com.github.arquiweb.fidelizacion.ejb;
+
+import com.github.arquiweb.fidelizacion.model.VencimientoPuntos;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class VencimientoPuntosDAO {
+
+    @PersistenceContext(unitName = "fidelizacionPU")
+
+    private EntityManager em;
+
+    public void agregar(VencimientoPuntos entidad) {
+        this.em.persist(entidad);
+    }
+
+    public VencimientoPuntos obtenerVencimiento(Integer idVencimiento){
+        return this.em.find(VencimientoPuntos.class, idVencimiento);
+    }
+
+    public void eliminar(Integer id) {
+        try {
+            VencimientoPuntos entity = obtenerVencimiento(id);
+            this.em.remove(entity);
+        } catch (Exception e) {
+            throw e;
+        }
+    }
+}

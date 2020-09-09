e52f7162117e9c483100b537834edccbc712997b
Javier Heisecke <javierheiseckee@gmail.com>
2020-09-08 22:15:21 -0400

    Agrega servicio de obtencion de reglas

--- src/main/java/com/github/arquiweb/fidelizacion/ejb/ReglaDAO.java
+++ src/main/java/com/github/arquiweb/fidelizacion/ejb/ReglaDAO.java
@@ -1,7 +1,10 @@
 package com.github.arquiweb.fidelizacion.ejb;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 import com.github.arquiweb.fidelizacion.model.Cliente;
 import com.github.arquiweb.fidelizacion.model.Regla;
@@ -12,6 +15,13 @@ public class ReglaDAO {
 	@PersistenceContext(unitName = "fidelizacionPU")
 	
     private EntityManager em;
+
+    public Object obtener() {
+    	List<Regla> reglas = null;
+        Query q = this.em.createQuery("select p from Regla p");
+        reglas = (List<Regla>) q.getResultList();    	
+        return reglas;    	
+    }  	
 	
     public void agregar(Regla entidad) {
         this.em.persist(entidad);

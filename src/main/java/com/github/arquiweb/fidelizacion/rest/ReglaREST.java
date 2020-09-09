1a31142bae4b779a7081eee1d729669bf142edea
Javier Heisecke <javierheiseckee@gmail.com>
2020-09-08 22:17:27 -0400

    Actualiza path para servicios de regla

--- src/main/java/com/github/arquiweb/fidelizacion/rest/ReglaREST.java
+++ src/main/java/com/github/arquiweb/fidelizacion/rest/ReglaREST.java
@@ -10,15 +10,12 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
 
-import com.github.arquiweb.fidelizacion.ejb.ClienteDAO;
 import com.github.arquiweb.fidelizacion.ejb.ReglaDAO;
-import com.github.arquiweb.fidelizacion.model.Cliente;
 import com.github.arquiweb.fidelizacion.model.Regla;
 
-@Path("cliente")
+@Path("regla")
 @Consumes("application/json")
 @Produces("application/json")
 public class ReglaREST {
